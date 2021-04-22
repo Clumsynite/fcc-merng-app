b22b80492d5c44b409d8a69b20435564ca4efa18
Rishabh Pathak <clusmsyknight@gmail.com>
2021-04-22 17:18:13 +0530

    added create post function

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -5,6 +5,7 @@ import {
   createHttpLink,
   ApolloProvider,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import App from "./App";
 
@@ -12,12 +13,19 @@ const httpLink = createHttpLink({
   uri: "http://localhost:5000",
 });
 
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
-
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default (
   <ApolloProvider client={client}>
