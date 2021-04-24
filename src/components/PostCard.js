5c80c7b77c6469d3aac8efb77c706511a6ba6ac7
Rishabh Pathak <clusmsyknight@gmail.com>
2021-04-24 17:43:47 +0530

    like a post
    added like functionality

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
 import moment from "moment";
 import { Link } from "react-router-dom";
+
+import LikeButton from "./LikeButton";
+
+import { AuthContext } from "../context/auth";
 
 function PostCard({
   post: {
@@ -15,13 +19,8 @@ function PostCard({
     likes,
   },
 }) {
-  const likePost = () => {
-    console.log("Like Post");
-  };
+  const { user } = useContext(AuthContext);
 
-  const commentOnPost = () => {
-    console.log("Comment on Post");
-  };
   return (
     <Card fluid>
       <Card.Content>
@@ -37,15 +36,8 @@ function PostCard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as="div" labelPosition="right" onClick={likePost}>
-          <Button color="teal" basic>
-            <Icon name="heart" />
-          </Button>
-          <Label basic color="teal" pointing="left">
-            {likeCount}
-          </Label>
-        </Button>
-        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+        <LikeButton post={{ id, likes, likeCount }} user={user} />
+        <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
           <Button color="blue" basic>
             <Icon name="comments" />
           </Button>
@@ -53,6 +45,16 @@ function PostCard({
             {commentCount}
           </Label>
         </Button>
+        {user && user.username === username && (
+          <Button
+            as="div"
+            color="red"
+            floated="right"
+            onClick={() => console.log("Delete post")}
+          >
+            <Icon name="trash" style={{ margin: 0 }} />
+          </Button>
+        )}
       </Card.Content>
     </Card>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { useQuery } from "@apollo/client";
-import { Grid } from "semantic-ui-react";
+import { Grid, Transition } from "semantic-ui-react";
 
 import { AuthContext } from "../context/auth";
 import { FETCH_POSTS_QUERY } from "../util/graphql";
@@ -27,12 +27,14 @@ function Home() {
         {loading ? (
           <h1>Loadin posts....</h1>
         ) : (
-          data?.getPosts &&
-          data?.getPosts.map((post) => (
-            <Grid.Column key={post.id} style={{ marginBottom: "4px" }}>
-              <PostCard post={post} />
-            </Grid.Column>
-          ))
+          <Transition.Group>
+            {data?.getPosts &&
+              data?.getPosts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: "4px" }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
         )}
       </Grid.Row>
     </Grid>
